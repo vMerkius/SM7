b8f375b7edd48d65a70ef682e42427904a5430c4
Merkius 1338 <merkius@wp.pl>
2022-12-06 02:45:13 +0100

    19

---
+++ app/src/main/java/com/example/booksearch/Book.java
@@ -0,0 +1,44 @@
+package com.example.booksearch;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Book {
+    @SerializedName("title")
+    private String title;
+    @SerializedName("author_name")
+    private List<String> authors;
+    @SerializedName("cover_i")
+    private String cover;
+    @SerializedName("number_of_pages_median")
+    private String numberOfPages;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getNumberOfPages() {
+        return numberOfPages;
+    }
+    public void setNumberOfPages(String numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+}
---
+++ app/src/main/java/com/example/booksearch/BookContainer.java
@@ -0,0 +1,17 @@
+package com.example.booksearch;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class BookContainer {
+    @SerializedName("docs")
+    private List<Book> bookList;
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+}
